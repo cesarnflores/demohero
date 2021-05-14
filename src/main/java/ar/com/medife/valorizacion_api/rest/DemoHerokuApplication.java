ac6d140db8976663f58c8ecb7f5be51afa778d0f
cesarnflores <36217657+cesarnflores@users.noreply.github.com>
2021-05-14 12:59:18 -0300

    Add files via upload

---
+++ src/main/java/ar/com/medife/valorizacion_api/rest/DemoHerokuApplication.java
@@ -0,0 +1,14 @@
+package ar.com.medife.valorizacion_api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DemoHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHerokuApplication.class, args);
+	}
+
+}
